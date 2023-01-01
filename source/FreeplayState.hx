998b7192fa968e1e1db0ad7baf71cefb5b298af2
Sopunic509 <121322637+Sopunic509@users.noreply.github.com>
2023-01-01 01:37:41 -0500

    Add files via upload

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -112,7 +112,7 @@ class FreeplayState extends MusicBeatState
 
 		for (i in 0...songs.length)
 		{
-			var songText:Alphabet = new Alphabet(0, (70 * i) + 30, songs[i].songName, true);
+			var songText:Alphabet = new Alphabet(90, -50, songs[i].songName, true);
 			songText.isMenuItem = true;
 			songText.targetY = i - curSelected;
 			grpSongs.add(songText);
